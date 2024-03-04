904e7ee8764f563769dd2b2cde327056cb2daf00
Yura <yuras92@ukr.net>
2024-03-04 11:12:53 +0200

    add loader

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -3,7 +3,7 @@ import CircularProgress from "@mui/material/CircularProgress";
 
 const Loader = () => {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
             <CircularProgress/>
         </div>
     );
